1f3812ed3b6927f27b0f6adb2f349c2ea26f7f13
Codie Snow <Csnow14@byui.edu>
2022-02-02 22:43:33 -0700

    Started work on parachute

---
+++ parachute.py
@@ -0,0 +1,11 @@
+class parachute:
+    def __init__(self):
+        mistakes = 0
+        #import word here
+        pass
+
+    def mistake_made(self):
+        self.mistake += 1
+        if self.mistake == 4:
+            #Game over
+            pass
