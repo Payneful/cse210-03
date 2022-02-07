12c77b0daffeb35a43eae56a70ac58244b6f1f3c
Codie Snow <Csnow14@byui.edu>
2022-02-07 12:52:37 -0700

    Added a draft of the stickman

--- parachute.py
+++ parachute.py
@@ -9,3 +9,20 @@ class parachute:
         if self.mistake == 4:
             #Game over
             pass
+
+
+
+
+"""
+    Word
+
+     ____   0 mistakes
+    /____\    1 mistakes
+    \    /   2 mistakes
+     \  /   3 mistakes
+      O     4 mistakes O -> X
+     /|\
+     / \
+
+^^^^^^^^^^^
+"""
