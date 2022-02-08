b9a1a6467b5ae9578d2456b6e26c927d9eb79eee
Codie Snow <Csnow14@byui.edu>
2022-02-08 10:34:43 -0700

    added stick figure

--- parachute.py
+++ parachute.py
@@ -1,34 +1,33 @@
-#from word import
 #from terminal_service import
 
 class parachute:
     def __init__(self):
-        mistakes = 0
-        #import word here
-        pass
+        self._mistakes = 0
 
-    def mistake_made(self):
-        self.mistake += 1
+    def add_mistake(self):
+        self._mistakes += 1
+
+    def create_stickman(self):
+        picture = "\n "
+        if self._mistakes == 0:
+            picture = picture + "  ___\n"
+        if self._mistakes < 2:
+            picture = picture + " /___\ \n"
+        if self._mistakes < 3:
+            picture = picture + " \   / \n"
+        if self._mistakes < 4:
+            picture = picture + "  \ / \n"
+        if self._mistakes == 4:
+            picture = picture + "   X \n"
+        else:
+            picture = picture + "   O \n"
+        picture = picture + "  /|\ \n  / \ \n"
+        #send picture to terminal
 
 
 
 
 """
-picture = ""
-if mistakes = 0:
-    picture = picture + "___\n"
-if mistakes < 2:
-    picture = picture + "/___\ \n"
-if mistakes < 3:
-    picture = picture + "\   / \n"
-if mistakes < 4:
-    picture = picture + "\  / \n"
-if mistakes = 4:
-    picture = picture + "X \n"
-else:
-    picture = picture + "O \n"
-picture = picture + "/|\ \n / \ "
-
     Word
 
      ____   0 mistakes
@@ -36,8 +35,8 @@ picture = picture + "/|\ \n / \ "
     \    /   2 mistakes
      \  /   3 mistakes
       O     4 mistakes O -> X
-     /|\
-     / \
+     /|\ 
+     / \ 
 
 ^^^^^^^^^^^
 """
