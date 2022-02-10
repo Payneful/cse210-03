06ef7556bd4b75098e14f324e902d3a8b5f68d0b
Codie Snow <Csnow14@byui.edu>
2022-02-10 13:21:23 -0700

    Fixed some things with stick figure and added terminal service

--- parachute.py
+++ parachute.py
@@ -1,14 +1,15 @@
-#from terminal_service import
+from terminal_service import Terminal_Service
 
 class parachute:
     def __init__(self):
         self._mistakes = 0
+        console = Terminal_Service()
 
     def add_mistake(self):
         self._mistakes += 1
 
     def create_stickman(self):
-        picture = "\n "
+        picture = "\n"
         if self._mistakes == 0:
             picture = picture + "  ___\n"
         if self._mistakes < 2:
@@ -23,6 +24,7 @@ class parachute:
             picture = picture + "   O \n"
         picture = picture + "  /|\ \n  / \ \n"
         #send picture to terminal
+        self.console.output(picture)
 
 
 
